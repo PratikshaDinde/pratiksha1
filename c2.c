36a479768df043c83c8ac301ab049b605dd6a6ce
PratikshaDinde <79303228+PratikshaDinde@users.noreply.github.com>
2021-02-28 03:26:31 -0800

    Create c2.c

---
+++ c2.c
@@ -0,0 +1,11 @@
+// program of swapping of nmbers
+#include<stdio.h>
+int main()
+{
+ int a=2,b=4;
+printf("Enter the value of a=%d and b=%d:\n\n",a,b);
+b=b-a; //  b becomes 2
+a=a+a; //a  becomes 4
+printf("Enter the value  after swapping:a=%d and b=%d",a,b);
+return 0;
+}
